2afc2eb3bba7b79137f6e35b6c1d3f1f0a06019a
Chad Smith <smith.chad12@gmail.com>
2023-09-01 23:33:05 -0500

    add comments

--- examples/equivalent_binary_trees/main.go
+++ examples/equivalent_binary_trees/main.go
@@ -6,6 +6,8 @@ import (
 	"golang.org/x/tour/tree"
 )
 
+// This will walk a tree recursively, inorder, and put the values in the channel
+// When it is done walking then it will close the channel to signal that nothing else is coming.
 func Walk(t *tree.Tree, ch chan int) {
     WalkRecursive(t, ch)
     close(ch)
@@ -19,6 +21,9 @@ func WalkRecursive(t *tree.Tree, ch chan int) {
     }
 }
 
+// This will take in two trees and walk them
+// If it finds a value in the two trees that don't belong then it will return false to signify they are not the Same
+// if we haven't exited yet when a channel has closed, then we know that they are the same, so return true.
 func Same(t1, t2 *tree.Tree) bool {
     ch1 := make(chan int)
     ch2 := make(chan int)
